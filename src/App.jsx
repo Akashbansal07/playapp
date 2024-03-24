f10944755489d9085eefc3afecc48d845416c63a
Akashbansal07 <142156032+Akashbansal07@users.noreply.github.com>
2024-03-24 14:43:46 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,12 @@ import Marcees from './components/Marcees';
 import Card from './components/Card';
 import Footer from './components/Footer';
 
-import LocomotiveScroll from 'locomotive-scroll';
+
 
 
 
 function App() {
-  const locomotiveScroll = new LocomotiveScroll();
+
   return (
     <div className='w-full h-full bg-zinc-900 text-white font-sans '>
       <Navbar />
